2a8913d77ea10aefd2eecc41d04be12a504e0546
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2018-08-03 16:35:41 +0530

    Add CRUD controls in view

--- client/main.js
+++ client/main.js
@@ -8,6 +8,31 @@ Todos = new Mongo.Collection('todos');
 // Template Helpers
 Template.main.helpers({
   todos() {
-    return Todos.find();
+    return Todos.find({}, {sort: {createdAt: -1}});
+  }
+});
+
+Template.main.events({
+  "submit .new-todo"(event) {
+    let text = event.target.text.value;
+
+    Todos.insert({
+      text: text,
+      createdAt: new Date()
+    });
+
+    // Clear Form
+    event.target.text.value = '';
+
+    // Prevent Submit
+    return false;
+  },
+  "click .toggle-checked"() {
+    Todos.update(this._id, {$set:{checked: ! this.checked}});
+  },
+  "click .delete-todo"() {
+    if(confirm('Are You Sure?')) {
+      Todos.remove(this._id);
+    }
   }
 });
